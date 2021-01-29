922fbee33d2cf01d16f405c15c6b7f4bb8fb80c7
Natalya Taranenko <go.avecanora@gmail.com>
2021-01-30 01:42:03 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -14,3 +14,21 @@
 // ];
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients.
 // Для создания DOM-узлов используй document.createElement().
+
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+];
+const liIngredients = document.createElement('li');
+for (const item of ingredients) { liIngredients.appendChild(document.createElement('ul')).classList.add(item); }
+// альтернатива циклу метод map
+// ingredients.map(function (element) {
+//     return liIngredients.appendChild(document.createElement('ul')).classList.add(element);
+// });
+
+
+console.log(liIngredients);
