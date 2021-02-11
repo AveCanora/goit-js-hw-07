51e892c01b0451f8263befaa39c7ba69095e9396
Natalya Taranenko <go.avecanora@gmail.com>
2021-02-11 22:50:00 +0200

    task-8

--- js/task-8.js
+++ js/task-8.js
@@ -23,14 +23,19 @@ const btnRender = document.querySelector('[data-action="render"]');
 const btnDestroy = document.querySelector('[data-action="destroy"]');
 const inputNumber = document.querySelector('[type="number"]');
 
+let widthDiv = 30;
+let heightDiv = 30;
+
 //функция создания дочерних элементов
 function createBoxes(amount) {
-    let widthDiv = 30;
-    let heightDiv = 30;
-    for (let i = 0; i < amount; i++) {
 
-        this.appendChild(document.createElement('div')).style = `width: ${widthDiv}px; height: ${heightDiv}px; background-color: 
-        rgb(${Math.random(100) * 100}%, ${Math.random(100) * 100}%, 1${Math.random(100) * 100}%);`;
+    for (let i = 0; i < amount; i++) {
+        this.appendChild(document.createElement('div')).style = `
+        width: ${widthDiv}px;
+        height: ${heightDiv}px; background-color: 
+        rgb(${Math.floor(Math.random() * 100)}%, ${Math.floor(Math.random() * 100)}%, ${Math.floor(Math.random() * 100)}%);
+        margin-left: auto;
+        margin-right: auto;`;
         widthDiv += 10;
         heightDiv += 10;
     }
@@ -46,6 +51,8 @@ btnRender.addEventListener('click', () => {
 //обработка события нажатия на кнопку "Очистить"
 btnDestroy.addEventListener('click', () => {
     destroyBoxes.call(divBoxes);
+    widthDiv = 30;
+    heightDiv = 30;
 });
 
 
