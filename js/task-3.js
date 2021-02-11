4b3495e47a68c386c41f8129f826b86ae9b802a0
Natalya Taranenko <go.avecanora@gmail.com>
2021-02-12 00:07:56 +0200

    prietter

--- js/task-3.js
+++ js/task-3.js
@@ -34,7 +34,7 @@
 import imagesObj from './images.js';
 const ulGallery = document.querySelector('ul#gallery');
 ulGallery.className = 'js-gallery';
-const elementLi = imagesObj.map(el => `<li class="js-gallery_li"><img src=${el.url} alt=${el.alt} width="400"></li>`).join('');
+const elementLi = imagesObj.map(el => `<li class="js-gallery_li"><img src=${el.url} alt=${el.alt} class="js-img"></li>`).join('');
 console.log(elementLi);
 ulGallery.insertAdjacentHTML('beforeend', elementLi);
 
