58abe6d9c3bd333245e62ef3da40ee6706286d5e
Natalya Taranenko <go.avecanora@gmail.com>
2021-02-19 00:41:14 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -39,11 +39,15 @@ let counterValue = 0;
 const buttonCounterInc = document.querySelector('[data-action="increment"]');
 const buttonCounterDec = document.querySelector('[data-action="decrement"]');
 const spanCounterDec = document.querySelector('[id="value"]');
-buttonCounterInc.addEventListener('click', () => {
+
+buttonCounterInc.addEventListener('click', increment);
+buttonCounterDec.addEventListener('click', decrement);
+
+function increment() {
   counterValue += 1;
   spanCounterDec.innerText = counterValue;
-});
-buttonCounterDec.addEventListener('click', () => {
+}
+function decrement() {
   counterValue -= 1;
   spanCounterDec.innerText = counterValue;
-});
+}
