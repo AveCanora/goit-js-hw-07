8a3f65b542746c01137421cdde0b63e6b07f470d
Natalya Taranenko <go.avecanora@gmail.com>
2021-02-19 19:35:39 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -25,8 +25,11 @@
 
 //РЕШЕНИЕ
 const inputText = document.querySelector('[id="validation-input"]');
+
 inputText.addEventListener('blur', () => {
-  if (inputText.value.length <= inputText.getAttribute('data-length')) {
+  if (
+    inputText.value.length === Number(inputText.getAttribute('data-length'))
+  ) {
     inputText.className = 'valid';
   } else {
     inputText.className = 'invalid';
